6c3ef60bfd4abfa6d313323a4dfcb430b25d1558
Eric J Wilhite <EricJWilhite@gmail.com>
2020-03-21 11:19:44 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    let formData = {
+        name: userName,
+        email: userEmail
+    }
+    let configObj = {
+        method: "post", 
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    }
+    return fetch("http://localhost:3000/users", configObj).then((response) => {
+        return response.json()
+    }).then((object) => {
+        let id = object.id
+        let body = document.querySelector('body')
+        body.innerHTML = id
+    }).catch((error) => {
+        let message = error.message
+        let body = document.querySelector('body')
+        body.innerHTML = message
+    })
+};
